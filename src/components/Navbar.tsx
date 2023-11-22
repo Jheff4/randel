5c2b546b816db27c8b42ec4737cb4d9bb1fb260c
Jheff4 <75406244+Jheff4@users.noreply.github.com>
2023-11-22 10:21:58 +0100

    started the landing page

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,170 @@
+"use client"
+
+import Image from 'next/image'
+import Link from 'next/link'
+import { Button } from './ui/button'
+import { HiMenu } from "react-icons/hi";
+import { HiX } from "react-icons/hi";
+import { useState } from 'react';
+
+const Navbar = () => {
+  const [navbar, setNavbar] = useState(true);
+
+  return (
+    <nav className='flex-center bg-blackPearl fixed top-0 z-20 w-full py-6 text-white'>
+      <div className={`${!navbar ? "flex justify-between max-md:flex-between max-md:flex-wrap mx-auto w-full max-w-screen-2xl lg:px-20 xs:px-8 sm:px-10 max-xs:px-3" : "flex-between mx-auto w-full transition-all max-w-screen-2xl lg:px-20 xs:px-8 sm:px-10 max-xs:px-3"}`}>
+        <Link href="/">
+          <Image
+            src="/logo.svg"
+            alt="logo"
+            width={120}
+            height={35}
+            className="max-md:hidden"
+          />
+          <Image
+            src="/Union.svg"
+            alt="logo"
+            width={40}
+            height={30}
+            className="block md:hidden order-1"
+          />
+        </Link>
+
+        {navbar ? (<ul className='flex-center gap-x-3 md:gap-x-6 -mr-12 max-md:hidden'>
+          <li className='!font-bold flex justify-center items-center px-4 py-[1.1rem] rounded-2xl bg-secondary'>
+            <Link
+              href=""
+              target=""
+            >
+              <Image
+                src="/docs.svg"
+                alt="docs"
+                width={20}
+                height={20}
+                className=""
+              />
+            </Link>
+          </li>
+          <li className='!font-bold flex justify-center items-center px-4 py-[0.9rem] rounded-2xl bg-secondary'>
+            <Link
+              href=""
+              target=""
+            >
+              <Image
+                src="/discord.svg"
+                alt="discord"
+                width={20}
+                height={20}
+                className=""
+              />
+            </Link>
+          </li>
+          <li className='!font-bold flex px-4 py-[0.95rem] rounded-2xl bg-secondary'>
+            <Link
+              href=""
+              target=""
+            >
+              <Image
+                src="/X.svg"
+                alt="twitter"
+                width={18}
+                height={18}
+                className=""
+              />
+            </Link>
+          </li>
+          <li className='!font-bold flex p-4 rounded-2xl bg-secondary'>
+            <Link
+              href=""
+              target=""
+            >
+              <Image
+                src="/telegram.svg"
+                alt="telegram"
+                width={18}
+                height={18}
+                className=""
+              />
+            </Link>
+          </li>
+        </ul>)
+          :
+        (<ul className='flex-center gap-x-10 max-md:order-3 max-md:bg-cardinalPink-100 max-md:h-screen max-md:flex-col max-md:gap-10 max-md:w-full max-md:justify-start max-md:pt-[5rem] max-md:flex-start relative'>
+          <li className='!font-bold max-md:hidden'>
+            <Link
+              href="#"
+              target="_blank"
+            >
+              Home
+            </Link>
+          </li>
+          <li className='!font-bold md:hidden'>
+            <Link
+              href="/dashboard"
+              target=""
+            >
+              Dashboard
+            </Link>
+          </li>
+          <li className='!font-bold md:hidden'>
+            <Link
+              href="/markets"
+              target=""
+            >
+              Markets
+            </Link>
+          </li>
+          <li className='!font-bold'>
+            <Link
+              href="https://lensly-finance.gitbook.io/lensly-document/"
+              target="#"
+            >
+              WhitePaper
+            </Link>
+          </li>
+          <li className='!font-bold md:hidden'>
+            <Link
+              href="/governance"
+              target=""
+            >
+              Governance
+            </Link>
+          </li>
+          <li className='!font-bold md:hidden'>
+            <Link
+              href="/stakes"
+              target=""
+            >
+              Stakes
+            </Link>
+          </li>
+          <li className='!font-bold'>
+            <Link
+              href=""
+              target=""
+            >
+              FAQ
+            </Link>
+          </li>
+        </ul>)
+        }
+
+        <div className='flex flex-row gap-[16px]'>  
+          <Button 
+            className={`${!navbar ? "p-button max-md:hidden" : "btn-gradient"}`}>
+              Join Community
+          </Button>
+
+          {navbar ? (<HiMenu 
+            className="block md:hidden bg-razzmatazz p-1 font-semibold rounded-[8px] text-[40px] cursor-pointer"
+            onClick={() => setNavbar(!navbar)}
+          /> ) : ( <HiX className="block md:hidden bg-razzmatazz p-1 font-semibold rounded-[8px] transition-all text-[40px] order-2 cursor-pointer"
+            onClick={() => setNavbar(!navbar)}
+          />) }
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,64 @@
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+    screens: {
+      xs: "400px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1220px",
+      "2xl": "1250px",
+      "3xl": "1700px",
+    },
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
       },
     },
+    extend: {
+      fontFamily: {
+        clash_grotesk: ["Clash Grotesk", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+      },
+      colors: {
+        primary: "#040722",
+        secondary: "rgba(255, 255, 255, 0.16)",
+        blackPearl: {
+          DEFAULT: "#040722",
+        },
+        ebonyClay: {
+          DEFAULT: "#2C2F45",
+        },
+        haiti: {
+          DEFAULT: "#121237",
+        },
+        white: {
+          DEFAULT: "#FFF",
+        }
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    }
   },
-  plugins: [],
-}
-export default config
+  plugins: [require("tailwindcss-animate")],
+};
